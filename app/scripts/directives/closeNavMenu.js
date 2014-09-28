88042e78e4f38211e626ac14df5d2e26ac28dfde
bgawel <bartek.gawel@gmail.com>
2014-09-28 21:45:25 +0200

    jhints

--- app/scripts/directives/closeNavMenu.js
+++ app/scripts/directives/closeNavMenu.js
@@ -4,7 +4,7 @@ angular.module('vegewroApp')
   .directive('closeNavMenu', function() {
     return {
       restrict: 'A',
-      link: function(scope, elem, attrs) {
+      link: function() {
         // https://github.com/twbs/bootstrap/issues/12852
         $(document).on('click', '.navbar-collapse.in', function(e) {
           if ($(e.target).is('a') || $(e.target).is('span')) {
